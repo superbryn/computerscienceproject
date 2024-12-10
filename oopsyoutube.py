31e06556c658d304d2081980a8984de1f645853f
xenogen <151805539+superbryn@users.noreply.github.com>
2024-12-11 02:08:32 +0530

    Update oopsyoutube.py

--- oopsyoutube.py
+++ oopsyoutube.py
@@ -62,29 +62,29 @@ class SpeedYUI:
         self.Widgets()
     
     def Widgets(self):
-        self.title = customtkinter.CTkLabel(self.master, text="Paste the URL here")
+        self.title = customtkinter.CTkLabel(self.app, text="Paste the URL here")
         self.title.pack(padx=10, pady=10)
 
         # Input Entry for URL
         self.link_variable = tkinter.StringVar()
-        self.link = customtkinter.CTkEntry(self.master, width=480, height=30, textvariable=self.link_variable)
+        self.link = customtkinter.CTkEntry(self.app, width=480, height=30, textvariable=self.link_variable)
         self.link.pack(padx=10, pady=10)
 
         # Segment Button (MP3/MP4)
-        self.datatype = customtkinter.CTkSegmentedButton(self.master, values=["MP3", "MP4"])
+        self.datatype = customtkinter.CTkSegmentedButton(self.app, values=["MP3", "MP4"])
         self.datatype.pack()
 
         # Download Button
-        self.download_button = customtkinter.CTkButton(self.master, text="Download", command=self.download, fg_color="red", hover="blue")
+        self.download_button = customtkinter.CTkButton(self.app, text="Download", command=self.download, fg_color="red", hover="blue")
         self.download_button.pack(padx=10, pady=10)
 
         # Debug Label
-        self.debug_label = customtkinter.CTkLabel(self.master, text="")
+        self.debug_label = customtkinter.CTkLabel(self.app, text="")
         self.debug_label.pack(padx=10)
 
         # Progress Bar (Currently not functional)
-        self.progress_bar = customtkinter.CTkLabel(self.master, text="0")
-        
+        self.progress_bar = customtkinter.CTkLabel(self.app, text="0")
+
         self.progress_bar.pack(padx=10)
 
         def download(self):
@@ -109,14 +109,6 @@ class SpeedYUI:
                 self.link.delete(0, customtkinter.END)  # Clear the URL input field
             except Exception as e:
                 self.debug_label.configure(text=f"Something went wrong: {e}")
-
-
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app = customtkinter.CTk()
