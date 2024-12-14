0af2e13291e5bed8c467655cadba8d888b12975c
xenogen <151805539+superbryn@users.noreply.github.com>
2024-12-14 08:19:28 +0530

    Update oopsyoutube.py

--- oopsyoutube.py
+++ oopsyoutube.py
@@ -1,11 +1,11 @@
-import customtkinter
+import customtkinter 
 from pytubefix import YouTube
-import tkinter
-from moviepy import VideoFileClip,AudioFileClip
+from PIL import Image
+from moviepy.editor import VideoFileClip,AudioFileClip
 import os
 import csv
 
-class YoutubeVideoDownloader:
+class YoutubeVideoDownloader: # The Downloader
     def __init__(self, url):
         self.ytUrl = url
         self.youtubeObject = None
@@ -20,7 +20,7 @@ class YoutubeVideoDownloader:
             self.catchLink()
             with open("logs.csv","a") as f:
                 csv.writer(f,delimiter=",").writerow(["mp4",self.youtubeObject.title,self.ytUrl])
-            videoStream = self.youtubeObject.streams.filter(resolution="1080p", progressive=False).first()
+            videoStream = self.youtubeObject.streams.filter(resolution="1440p", progressive=False).first()
             if not videoStream:
                 videoStream = self.youtubeObject.streams.filter(progressive=True).order_by('resolution').desc().first()
             audioStream = self.youtubeObject.streams.filter(only_audio=True).first()
@@ -32,8 +32,8 @@ class YoutubeVideoDownloader:
                 audio = AudioFileClip(audio_path)
 
                 video_clip = video.set_audio(audio)
-                combined_path = os.path.join(f"{self.youtubeObject.title}.mp4")
-                video_clip.write_videofile(combined_path, codec='libx264',audio_encode='aac')
+                combined_path = os.path.join(f"{self.youtubeObject.title}-SPEEDY.mp4")
+                video_clip.write_videofile(combined_path)
 
                 os.remove(audio_path)
                 os.remove(video_path)
@@ -53,12 +53,14 @@ class YoutubeVideoDownloader:
         except Exception as e:
             return f"Error while The video as MP3: {e}"
         
-class SpeedYUI:
+class SpeedYUI: #UI
     def __init__(self,app):
         self.app = app
-        self.app.geometry("720x720")
+        self.app.geometry("750x500")
         self.app.resizable(False,False)
-        self.app.title("Youtube Vid Downloader")
+        self.app.title("SPEEDY CONVERTER")
+        self.logoPath= os.path.abspath('SPEEDY/icon.ico')
+        self.app.iconbitmap(self.logoPath)
         customtkinter.set_default_color_theme("blue")
         customtkinter.set_appearance_mode("dark")
 
@@ -67,48 +69,73 @@ class SpeedYUI:
         self.Widgets()
     
     def Widgets(self):
-        self.title = customtkinter.CTkLabel(self.app, text="Paste the URL here")
-        self.title.pack(padx=10, pady=10)
+        self.background_image_path = os.path.abspath("SPEEDY/new.png")
+        print(self.background_image_path)
+        if os.path.exists(self.background_image_path):
+            self.myImage = customtkinter.CTkImage(dark_image=Image.open(self.background_image_path), size=(750, 500))
+            self.imageLabel = customtkinter.CTkLabel(self.app, image=self.myImage, text="")
+            self.imageLabel.place(relwidth=1, relheight=1)
+        else:
+            print("bg image not found")
 
-        self.link_variable = tkinter.StringVar()
-        self.link = customtkinter.CTkEntry(self.app, width=480, height=30, textvariable=self.link_variable)
-        self.link.pack(padx=10, pady=10)
 
-        self.datatype = customtkinter.CTkSegmentedButton(self.app, values=["MP3", "MP4"])
+        self.retro_font = ("Harlow Solid Italic", 48, "bold") #OCR A Extended
+        self.title = customtkinter.CTkLabel(self.app,text=" =SPEEDY= ",font=self.retro_font,bg_color="#FDAF3E")
+        self.title.pack(padx=0,pady=(170,0))
+
+        self.link = customtkinter.CTkEntry(self.app,
+                                           width=300, height=30, 
+                                           placeholder_text="Paste The URL Here",
+                                           bg_color="#FDAF3E",
+                                           fg_color="white",
+                                           border_color="white",
+                                           text_color="black")
+        self.link.pack(padx=0, pady=(10,10))
+
+        self.datatype = customtkinter.CTkSegmentedButton(self.app, values=["MP3", "MP4"],
+                                                        bg_color="#FDAF3E",
+                                                        selected_color="red",
+                                                        selected_hover_color="red",
+                                                        unselected_color="#FDAF3E",
+                                                        unselected_hover_color="#FDAF3E",
+                                                        fg_color="#FDAF3E",
+                                                        text_color="black"
+                                                        )
         self.datatype.pack()
 
-        self.download_button = customtkinter.CTkButton(self.app, text="Download", command=self.download, fg_color="red", hover="blue")
+        self.download_button = customtkinter.CTkButton(self.app, text="Download",
+                                                       command=self.download,
+                                                       fg_color="red",
+                                                       hover="blue",
+                                                       bg_color="#FDAF3E")
         self.download_button.pack(padx=10, pady=10)
 
-        self.debug_label = customtkinter.CTkLabel(self.app, text="")
+        self.debug_label = customtkinter.CTkLabel(self.app, text="",bg_color="#FDAF3E")
         self.debug_label.pack(padx=10)
 
-        self.progress_bar = customtkinter.CTkLabel(self.app, text="0")
-
-        self.progress_bar.pack(padx=10)
 
     def download(self):
         try:
             url = self.link.get()
             if not url:
-                self.debug_label.configure(text="Please enter a URL.")
+                self.debug_label.configure(text="Please enter a URL")
                 return
-            
             self.downloader = YoutubeVideoDownloader(url)
 
             datatype = self.datatype.get()
             if datatype == "MP4":
-                result = self.downloader.mp3downloader()
-            elif datatype == "MP3":
                 result = self.downloader.mp4downloader()
+            elif datatype == "MP3":
+                result = self.downloader.mp3downloader()
             else:
                 self.debug_label.configure(text="Please select a valid format (MP3 or MP4).")
                 return
 
             self.debug_label.configure(text=result)
-            self.link.delete(0, customtkinter.END)  # Clear the URL input field
+            self.link.delete(0, customtkinter.END)
         except Exception as e:
             self.debug_label.configure(text=f"Something went wrong: {e}")
+            
 
 if __name__ == "__main__":
     app = customtkinter.CTk()
